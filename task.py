ea23d9ea7b344c707c9df77ef1280aeefc791969
zarif25 <2502.zarif@gmail.com>
2022-07-23 13:06:04 +0600

    sterilize json

--- task.py
+++ task.py
@@ -1,7 +1,50 @@
-from typing import List
+from typing import Any, Dict, List, Tuple
+from datetime import datetime
+from typing_extensions import Self
 
 
 class Task:
-    def __init__(self, parent, children: List) -> None:
-        self.parent: Task = parent
+    def __init__(
+        self,
+        name: str,
+        parent: Self,
+        children: List[Self],
+        time_frames=None,
+    ) -> None:
+        self.name = name
+        self.parent: Task | None = parent
         self.children: List[Task] = children
+        self.time_frames: List[Tuple[datetime, datetime | None]] = time_frames or []
+
+    @property
+    def to_json_dict(self) -> Dict[str, Any]:
+        data = self.__dict__.copy()
+        data.update(
+            {
+                "time_frames": [
+                    (
+                        i.strftime("%Y-%m-%d %H:%M:%S"),
+                        j.strftime("%Y-%m-%d %H:%M:%S") if j else None,
+                    )
+                    for i, j in self.time_frames
+                ],
+                "children": [i.to_json_dict for i in self.children]
+            }
+        )
+        return data
+
+    @classmethod
+    def from_json_dict(cls, data: Dict[str, Any]) -> Self:
+        data.update(
+            {
+                "time_frames": [
+                    (
+                        datetime.strptime(i, "%Y-%m-%d %H:%M:%S"),
+                        datetime.strptime(j, "%Y-%m-%d %H:%M:%S") if j else None,
+                    )
+                    for i, j in data["time_frames"]
+                ],
+                "children": [cls.from_json_dict(i) for i in data["children"]]
+            }
+        )
+        return cls(**data)

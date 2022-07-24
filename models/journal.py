ad67c76b47a6d78da1d608e019050bd6654bc0ba
zarif25 <2502.zarif@gmail.com>
2022-07-24 16:33:31 +0600

    save journal

--- models/journal.py
+++ models/journal.py
@@ -12,7 +12,7 @@ class Journal:
     def __init__(self, activities: List[Activity], date: datetime.date) -> None:
         self.date = date
         self.activities = activities
-        self.filepath = os.path.join(DB_DIR, "journal", date.year, date.month, f"{date.day}.json")
+        self.filepath = os.path.join(DB_DIR, "journal", str(date.year), str(date.month), f"{date.day}.json")
 
     @property
     def to_py_obj(self):
@@ -28,12 +28,23 @@ class Journal:
         return cls(**data)
 
     @classmethod
+    def create_month_dir_if_does_not_exist(self, date: datetime.date):
+        month_dir = os.path.join(DB_DIR, "journal", str(date.year), str(date.month))
+        if not os.path.exists(month_dir):
+            os.makedirs(month_dir)
+
+    @classmethod
     def from_json_file(cls, date: datetime.date):
-        filepath = os.path.join(DB_DIR, "journal", date.year, date.month, f"{date.day}.json")
+        filepath = os.path.join(DB_DIR, "journal", str(date.year), str(date.month), f"{date.day}.json")
+        cls.create_month_dir_if_does_not_exist(date)
+        if not os.path.exists(cls.filepath):
+            with open(cls.filepath, "w") as file:
+                json.dump([], file)
         with open(filepath, "r") as file:
             data = json.load(file)
         return cls.from_py_obj(data)
     
     def save(self):
+        self.create_month_dir_if_does_not_exist(self.date)
         with open(self.filepath, 'w') as file:
             json.dump(self.to_py_obj, file)

9e2cad7c0f10652172ff99de393e6b1dce4cd969
zarif25 <2502.zarif@gmail.com>
2022-07-25 15:38:20 +0600

    emoji const

--- models/task.py
+++ models/task.py
@@ -4,6 +4,8 @@ from typing_extensions import Self
 from rich.tree import Tree
 
 DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+EMOJI_INCOMPLETE_TASK = '⬛'
+EMOJI_COMPLETE_TASK = '✅'
 
 
 class Task:
@@ -55,7 +57,7 @@ class Task:
         return cls(**data)
 
     def tree_str(self):
-        return Tree(f"{'✅' if self.isDone else '⬛'} {self.name}")
+        return Tree(f"{EMOJI_COMPLETE_TASK if self.isDone else EMOJI_INCOMPLETE_TASK} {self.name}")
 
     def path(self):
         if self.parent == None:
